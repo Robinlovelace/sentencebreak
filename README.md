c548869df59f1289ce41279bdfd16f4cd6195fdc
robinlovelace <rob00x@gmail.com>
2023-05-19 22:36:50 +0100

    Add basic (not working) example

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sentencebreak
-Command line tool to break lines at sentences
+
+Command line tool to break lines at sentences.
+
+This Rust crate was created in a new folder with the following command:
+
+```bash
+cargo init
+```
---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use regex::Regex;
+use std::env;
+use std::fs;
+
+fn main() {
+    // Read command-line arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: sentencebreak <input-file>");
+        return;
+    }
+    let input_file = &args[1];
+
+    // Read input text
+    let input_text = match fs::read_to_string(input_file) {
+        Ok(text) => text,
+        Err(err) => {
+            println!("Error reading input file: {}", err);
+            return;
+        }
+    };
+
+    // Add line breaks after each sentence
+    let output_text = add_line_breaks(&input_text);
+
+    // Write modified text to a new file
+    let output_file = format!("{}_modified.txt", input_file);
+    match fs::write(&output_file, output_text) {
+        Ok(_) => println!("Modified text written to {}", output_file),
+        Err(err) => println!("Error writing output file: {}", err),
+    }
+}
+
+fn add_line_breaks(text: &str) -> String {
+    let re = Regex::new(r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s").unwrap();
+    re.replace_all(text, "$0\n").to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_line_breaks() {
+        let input_text = "This is a sentence. This is another sentence? This is a third sentence.";
+        let expected_output = "This is a sentence.\nThis is another sentence?\nThis is a third sentence.";
+        assert_eq!(add_line_breaks(input_text), expected_output);
+    }
+}
